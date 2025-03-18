105bd6b066eddc854dfb2c6287fb4026f4891cc6
igcarvalhaes <igcarvalhaes@hotmail.com>
2025-03-18 13:46:24 -0300

    My first components

---
+++ src/Greeting.jsx
@@ -0,0 +1,10 @@
+export function Greeting() {
+  return (
+    <h1>
+      &quot;Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
+      debitis, animi earum modi architecto impedit placeat consequuntur incidunt
+      nobis unde deserunt at corporis dignissimos tempore veniam tenetur
+      voluptates eum nulla!&quot;
+    </h1>
+  );
+}
---
+++ src/components/Food.jsx
@@ -0,0 +1,29 @@
+import Style from "./Food.module.css";
+
+export function Food() {
+  return (
+    <ul>
+      <li>
+        <img
+          src="https://images.unsplash.com/photo-1742054292312-7bd7d3984b40?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Eggs"
+          srcset=""
+        />
+      </li>
+      <li>
+        <img
+          src="https://plus.unsplash.com/premium_photo-1671397681247-abe9cea66a11?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Mushrooms"
+          srcset=""
+        />
+      </li>
+      <li>
+        <img
+          src="https://images.unsplash.com/photo-1496116218417-1a781b1c416c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Gyoza"
+          srcset=""
+        />
+      </li>
+    </ul>
+  );
+}
